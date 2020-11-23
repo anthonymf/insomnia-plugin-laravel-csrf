05cb02b31d6ed731e4f4052f9dcee1675476bfb7
Vincent Chi <vincent@rosetta.ai>
2020-11-23 15:38:12 +0800

    feat: get current workspace.

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ module.exports.templateTags = [{
     displayName: 'Laravel CSRF',
     description: 'Apply XSRF-TOKEN from cookie for X-XSRF-TOKEN',
     async run(context) {
-        return 'Hello World';
+        const { meta } = context;
+
+        if (!meta.requestId || !meta.workspaceId) {
+            return null;
+        }
+
+        const workspace = await context.util.models.workspace.getById(meta.workspaceId);
+
+        if (!workspace) {
+            throw new Error(`Workspace not found for ${meta.workspaceId}`);
+        }
     }
 }];
